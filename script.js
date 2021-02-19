4692ac1d8cb90a8ca3da682ab92ad7de38514a3c
tourdetravoy <dooganb@gmail.com>
2021-02-19 18:40:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ info.update = function (props) {
   this._div.innerHTML +=  (props
     ? '<b>' + props.name + '</b><br />' + value + '</b><br />'
       + (props.year ? 'Most recent data: ' + props.year : '')
-      : 'Hover over nations');
+    : 'Hover over nations');
 };
 info.addTo(map);
 
