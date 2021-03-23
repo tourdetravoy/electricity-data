f77f4e4eee06bdfaea64c3f3623115d10a1692c0
tourdetravoy <dooganb@gmail.com>
2021-03-23 14:37:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ info.update = function (props) {
   this._div.innerHTML +=  (props
     ? '<b>' + props.name + '</b><br />' + value + '</b><br />'
       + (props.year ? 'Most recent data: ' + props.year : '')
-      + (props.solar ? 'Solarpower: ' + props.year : '')                     
+      + (props.solar ? 'Solarpower: ' + props.solar : '%')                     
     : 'Hover over nations');
 };
 info.addTo(map);
